ce1f2a8bf8e10ac0c7f4a2aa8a3607fc68116aba
Daaizo <danielcharlak@wp.pl>
2023-06-05 17:52:31 +0200

    refactoring

--- src/main/java/com/example/is_charlak_brodka/util/CSVHelper.java
+++ src/main/java/com/example/is_charlak_brodka/util/CSVHelper.java
@@ -17,25 +17,10 @@ public class CSVHelper {
         Map<String, Double> unemploymentData = getUnemploymentData();
         Map<String, List<Double>> mergedData = new HashMap<>();
 
-        covidData.forEach((key, value) -> {
-            if (mergedData != null) {
-                if (mergedData.get(key)!= null) {
-                    mergedData.get(key).add(value);
-                } else {
-                    mergedData.put(key, new ArrayList<>(Collections.singleton(value)));
-                }
-            }
-        });
-        unemploymentData.forEach((key, value) -> {
-            if (mergedData != null) {
-                if (mergedData.get(key)!= null) {
-                    mergedData.get(key).add(value);
-                } else {
-                    mergedData.put(key, new ArrayList<>(Collections.singleton(value)));
-                }
-            }
-        });
+        mergeData(covidData, mergedData);
+        mergeData(unemploymentData, mergedData);
         ArrayList<GraphData> graphDataList = new ArrayList<>();
+
         mergedData.forEach((key, doubles) -> {
             if (doubles.size() == 2) {
                 String country = key.split("_")[0];
@@ -48,9 +33,19 @@ public class CSVHelper {
 
     }
 
+    private static void mergeData(Map<String, Double> covidData, Map<String, List<Double>> mergedData) {
+        covidData.forEach((key, value) -> {
+            if (mergedData.get(key) != null) {
+                mergedData.get(key).add(value);
+            } else {
+                mergedData.put(key, new ArrayList<>(Collections.singleton(value)));
+            }
+        });
+    }
+
     private static Map<String, Double> getCovidData() {
         enum CovidDataHeaders {
-            Date_reported,Country_code,Country,WHO_region,New_cases,Cumulative_cases,New_deaths,Cumulative_deaths
+            Date_reported, Country_code, Country, WHO_region, New_cases, Cumulative_cases, New_deaths, Cumulative_deaths
         }
         String csvFilePath2 = "src/main/resources/static/WHO-COVID-19-global-data.csv";
         CSVFormat csvFormat2 = CSVFormat.DEFAULT.builder()
@@ -70,18 +65,8 @@ public class CSVHelper {
                 String month = time.split("-")[1];
                 String year = time.split("-")[0];
                 String key = location + "_" + month + "_" + year;
-                data.put(key, data.getOrDefault(key, 0.00) +  Double.parseDouble(value));
+                data.put(key, data.getOrDefault(key, 0.00) + Double.parseDouble(value));
             }
-
-            // Print the extracted data
-//            for (Map.Entry<String, Integer> entry : data.entrySet()) {
-//                String key = entry.getKey();
-//                String country = key.split("_")[0];
-//                String month = key.split("_")[1];
-//                String year = key.split("_")[2];
-//                int casesSum = entry.getValue();
-//                System.out.printf("Country: %s, Month: %s, Year: %s Cases: %d%n", country, month,year, casesSum);
-//            }
             return data;
 
         } catch (IOException e) {
@@ -92,7 +77,7 @@ public class CSVHelper {
 
     private static Map<String, Double> getUnemploymentData() {
         enum UnemploymentDataHeaders {
-            LOCATION,INDICATOR,SUBJECT,MEASURE,FREQUENCY,TIME,Value
+            LOCATION, INDICATOR, SUBJECT, MEASURE, FREQUENCY, TIME, Value
         }
 
         String csvFilePath = "src/main/resources/static/unemployment_data_wordlwide.csv";
@@ -112,18 +97,9 @@ public class CSVHelper {
                 String month = time.split("-")[1];
                 String year = time.split("-")[0];
                 String key = location + "_" + month + "_" + year;
-                data.put(key,data.getOrDefault(key, 0.00) +  Double.parseDouble(value));
+                data.put(key, data.getOrDefault(key, 0.00) + Double.parseDouble(value));
 
             }
-            // Print the extracted data
-//            for (Map.Entry<String, Integer> entry : data.entrySet()) {
-//                String key = entry.getKey();
-//                String country = key.split("_")[0];
-//                String month = key.split("_")[1];
-//                String year = key.split("_")[2];
-//                int casesSum = entry.getValue();
-//                System.out.printf("Country: %s, Month: %s, Year: %s Cases: %d%n", country, month,year, casesSum);
-//            }
             return data;
 
         } catch (IOException e) {
