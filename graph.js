b430776c1b4c74dc92a1931cd6c93b5f2e3ddeef
maciekb23 <maciekb23@gmail.com>
2023-06-06 18:26:11 +0200

    Added filtering by year, beginning to end. Added XML downloads for each chart. Added full data download JSON and XML.

--- graph.js
+++ graph.js
@@ -1,7 +1,22 @@
-let json1;
-let json2;
-let region1;
-let region2
+let json1, json2, region1, region2, startDate, endDate, xml1, xml2, jsonFull, xmlFull;
+async function loadData(){
+
+    $.ajax({
+        url: "http://localhost:8080/graphData",
+        type: 'GET',
+        success: function(res) {
+            console.log(res)
+            jsonFull = res;
+        }});
+
+    await $.ajax({
+        url: "http://localhost:8080/graphData/xml",
+        type: 'GET',
+        success: function(res) {
+            console.log(res);
+            xmlFull = res;
+        }});
+}
 
 async function test() {
     let xValues = [];
@@ -10,14 +25,18 @@ async function test() {
     let chart1;
     region1 = $("#region1")[0].value;
     region2 = $("#region2")[0].value;
+    startDate = $('#startDate')[0].value;
+    endDate = $('#endDate')[0].value;
 
     await $.ajax({
-        url: "http://localhost:9192/graphData?countryCode="+region1,
+        url: "http://localhost:8080/graphData?yearStart="+startDate+"&yearEnd="+endDate+"&countryCode="+region1,
         type: 'GET',
         success: function(res) {
+            console.log(res);
+
             json1 = res;
             res.sort(function(a,b){
-                return a.year.localeCompare(b.year) || a.month.localeCompare(b.month)
+                return a.year - b.year || a.month - b.month;
             })
             xValues = [];
             yValues1 = [];
@@ -34,7 +53,7 @@ async function test() {
     $('#canvas1')[0].innerHTML = ''
     $('#canvas1')[0].innerHTML = '<canvas id="chart1" style="width:100%;max-width:700px"></canvas>'
     $('#region1header')[0].innerHTML = 'Wykres dla '+region1
-    chart1 = new Chart("chart1", {
+    new Chart("chart1", {
         type: "line",
         data: {
             labels: xValues,
@@ -74,13 +93,14 @@ async function test() {
         }
     });
 
+
     await $.ajax({
-        url: "http://localhost:9192/graphData?countryCode="+region2,
+        url: "http://localhost:8080/graphData?yearStart="+startDate+"&yearEnd="+endDate+"&countryCode="+region2,
         type: 'GET',
         success: function(res) {
             json2 = res;
             res.sort(function(a,b){
-                return a.year.localeCompare(b.year) || a.month.localeCompare(b.month)
+                return a.year - b.year || a.month - b.month;
             })
             xValues = [];
             yValues1 = [];
@@ -96,7 +116,7 @@ async function test() {
     $('#canvas2')[0].innerHTML = ''
     $('#canvas2')[0].innerHTML = '<canvas id="chart2" style="width:100%;max-width:700px"></canvas>'
     $('#region2header')[0].innerHTML = 'Wykres dla '+region2
-    chart1 = new Chart("chart2", {
+    new Chart("chart2", {
         type: "line",
         data: {
             labels: xValues,
@@ -135,8 +155,26 @@ async function test() {
             }
         }
     });
-}
 
+
+        await $.ajax({
+            url: "http://localhost:8080/graphData/xml?yearStart="+startDate+"&yearEnd="+endDate+"&countryCode="+region1,
+            type: 'GET',
+            success: function(res) {
+                console.log(res);
+                xml1 = res;
+            }
+    });
+        await $.ajax({
+        url: "http://localhost:8080/graphData/xml?yearStart="+startDate+"&yearEnd="+endDate+"&countryCode="+region2,
+        type: 'GET',
+        success: function(res) {
+            console.log(res);
+            xml2 = res;
+        }
+    });
+
+}
 function exportJsonRegion1(el) {
     var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json1));
     el.setAttribute("href", "data:"+data);
@@ -144,8 +182,41 @@ function exportJsonRegion1(el) {
 }
 
 
+function exportXMLRegion1(el) {
+    const serializer = new XMLSerializer();
+    const xmlStr = serializer.serializeToString(xml1);
+    var data = "text/xml;charset=utf-8," + encodeURIComponent(xmlStr);
+    el.setAttribute("href", "data:"+data);
+    el.setAttribute("download", "data"+region1+".xml");
+}
+
+
 function exportJsonRegion2(el) {
     var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json2));
     el.setAttribute("href", "data:"+data);
     el.setAttribute("download", "data"+region2+".json");
+}
+
+
+ function exportXMLRegion2(el) {
+        const serializer = new XMLSerializer();
+        const xmlStr = serializer.serializeToString(xml2);
+        var data = "text/xml;charset=utf-8," + encodeURIComponent(xmlStr);
+        el.setAttribute("href", "data:"+data);
+        el.setAttribute("download", "data"+region2+".xml");
+    }
+
+
+ async function exportJsonFull(el){
+     var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(jsonFull));
+     el.setAttribute("href", "data:"+data);
+     el.setAttribute("download", "dataFull.json");
+    }
+
+function exportXMLFull(el) {
+    const serializer = new XMLSerializer();
+    const xmlStr = serializer.serializeToString(xmlFull);
+    var data = "text/xml;charset=utf-8," + encodeURIComponent(xmlStr);
+    el.setAttribute("href", "data:"+data);
+    el.setAttribute("download", "dataXMLFull");
 }
